b9cbc76b2a12945e7f51fb5eb61b0ffc3ff3213c
miohitokiri5474 <lltzpp@gmail.com>
2022-10-12 22:19:53 +0800

    I hate micro

--- micro/lab04/bouns.asm
+++ micro/lab04/bouns.asm
@@ -3,21 +3,11 @@ LIST p=18f4520
 	CONFIG OSC = INTIO67
 	CONFIG WDT = OFF
 	org 0x00
-
-Next macro x1
-    clrf WREG
-    addwf 0x002, W
-    addwf 0x002
-    goto Hanoi
-    endm
 	
 init:
-    clrf TRISC
     clrf WREG
-    movlw 0x03
+    movlw 0x04
     movwf TRISA
-    movlw 0x01
-    movwf TRISB
     call Hanoi
     
     goto endl
