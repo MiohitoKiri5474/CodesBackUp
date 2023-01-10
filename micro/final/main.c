3c160e0a7003868b612482d6d10e73288ee8b0f8
Miohito Kiri <lltzpp@gmail.com>
2023-01-10 16:49:47 +0800

    it can works!

--- micro/final/main.c
+++ micro/final/main.c
@@ -24,9 +24,9 @@
 #define _XTAL_FREQ 4000000
 
 
-int enable = 1; // 可變電阻是否有作用
-int start = 0; // 是否開始
-int blink = 1; // 是否閃爍
+int enable = 1; // ?????????
+int start = 0; // ????
+int blink = 1; // ????
 int blink_status = 0; // 0: disable, 1: enable
 int times; // 1 ~ 60 sec
 
@@ -40,19 +40,17 @@ int min ( int a, int b ) {
 void display ( int num ) {
 	int res;
 	switch ( num % 10 ) {
-		case 0: res = 0b11000000; break;
-		case 1: res = 0b11111001; break;
-		case 2: res = 0b10100100; break;
-		case 3: res = 0b10110000; break;
-		case 4: res = 0b10011001; break;
-		case 5: res = 0b10010010; break;
-		case 6: res = 0b10000010; break;
-		case 7: res = 0b11111000; break;
-		case 8: res = 0b10000000; break;
-		case 9: res = 0b10010000; break;
-		default: res = 0b00000000; break;
+		case 0: res = 0b00111111; break;
+		case 1: res = 0b00000110; break;
+		case 2: res = 0b01011011; break;
+		case 3: res = 0b01001111; break;
+		case 4: res = 0b01100110; break;
+		case 5: res = 0b01101101; break;
+		case 6: res = 0b01111101; break;
+		case 7: res = 0b00000111; break;
+		case 8: res = 0b01111111; break;
+		case 9: res = 0b01101111; break;
 	}
-	res ^= 0b11111111; // use if need
 	low = res;
     switch ( num / 10 ) {
 		case 0:  res = 0b11000000; break;
@@ -65,9 +63,8 @@ void display ( int num ) {
 		case 7:  res = 0b11111000; break;
 		case 8:  res = 0b10000000; break;
 		case 9:  res = 0b10010000; break;
-		default: res = 0b00000000; break;
 	}
-	res ^= 0b11111111; // use if need
+	//res ^= 0b11111111; // use if need
 	high = res;
 }
 
@@ -97,8 +94,8 @@ void main ( void ) {
     ADCON1bits.VCFG = 0b00; // VREF+ = VDD VREF- = VSS
     ADCON1bits.PCFG = 0b1110; // AN0(RA0)=A
     ADCON0bits.CHS = 0b0000; // analog input = RA0
-    ADCON2bits.ADCS = 0b001; // Fosc = 4MHz, 8*1/4M = 2 mu s，算Tad
-    ADCON2bits.ACQT = 0b100; // Tad = 2, AQtime 4tad = 8 > 2.4，幾次一輪迴
+    ADCON2bits.ADCS = 0b001; // Fosc = 4MHz, 8*1/4M = 2 mu s??Tad
+    ADCON2bits.ACQT = 0b100; // Tad = 2, AQtime 4tad = 8 > 2.4??????
     ADCON2bits.ADFM = 1; // 0~1023 = 10 bits, right justified
     ADCON0bits.ADON = 1; // open A/D convert bit
     
@@ -115,53 +112,61 @@ void main ( void ) {
     // RB0: set time
     // RB1: start or not
     while ( 1 ) {
-    }
-}
-
-void __interrupt(high_priority) ADC_ISR (){
-    if ( enable ) {
-    	unsigned int number = 0;
-    	number |= ADRESH;
-    	number <<= 8;
-    	number |= ADRESL;
-    	times = min ( number / 17 + 1, 60 );
-    	PIR1bits.ADIF = 0;
-    	ADCON0bits.GODONE = 1;
-		display ( times );
-    	// LATC = high;
-    	LATC = start;
-    	LATD = low;
-    	__delay_us ( 4 );
-	}
-	if ( !PORTBbits.RB0 ) {
-        enable ^= 1;
-        __delay_us ( 4 );
-    }
-    if ( !PORTBbits.RB1 ) {
-        start ^= 1;
-		blink ^= 1;
-        if ( !start )
-            __delay_us ( 4 );
-    }
-    if ( start ) {
-        // running
-        __delay_us ( 4 );
-        cnt++;
-        if ( cnt == 250000 ) {
-            times--, cnt = 0;
-			display ( times );
-			// LATC = high;
-            LATD = low;
-		}
-    }
-	else {
-        __delay_us ( 4 );
-        blink_cnt++;
-        if ( blink_cnt == 25000 ) {
-            blinking();
-            blink_cnt = 0;
+        if ( !PORTBbits.RB0 ) {
+            enable ^= 1;
+            __delay_us ( 1000 );
         }
-	}
+        if ( !PORTBbits.RB1 && !enable ) {
+            start ^= 1;
+            blink ^= 1;
+            __delay_us ( 1000 );
+            LATC = high;
+            LATD = low;
+        }
+        if ( enable ) {
+            unsigned int number = 0, deg = 17;
+            number |= ADRESH;
+            number <<= 8;
+            number |= ADRESL;
+            times = 0;
+            while ( number > deg ) {
+                times++;
+                number -= deg;
+            }
+            PIR1bits.ADIF = 0;
+            ADCON0bits.GODONE = 1;
+            display ( times );
+            LATC = high;
+            LATD = low;
+            __delay_us ( 4 );
+        }
+        else {
+            if ( start ) {
+                // running
+                __delay_us ( 1000 );
+                cnt++;
+                if ( cnt == 1000 ) {
+                    times--, cnt = 0;
+                    display ( times );
+                    LATC = high;
+                    LATD = low;
+                }
+                if ( !times ) {
+                    start ^= 1;
+                    blink ^= 1;
+                }
+            }
+            else {
+                __delay_us ( 1000 );
+                blink_cnt++;
+                if ( blink_cnt == 100 ) {
+                    blinking();
+                    blink_cnt = 0;
+                }
+            }
+        }
+        display ( times );
+    }
 }
 
 // TIRSA: analog input
