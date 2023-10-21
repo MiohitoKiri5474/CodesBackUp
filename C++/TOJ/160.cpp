e199a511c03c8500ece55a46541b96e6f909dea2
miohitokiri5474 <lltzpp>
2023-10-21 14:55:48 +0800

    slved some problem

--- C++/TOJ/160.cpp
+++ C++/TOJ/160.cpp
@@ -93,6 +93,13 @@ int main(){
 			dp[u][v] = min ( dp[u][v], q );
 		}
 
+		cout << "---\n";
+		for ( int i = 1 ; i <= n ; i++ ) {
+			for ( int j = 1 ; j <= n ; j++ )
+				cout << dp[i][j] << ' ';
+			cout << endl;
+		}
+
 		for ( int k = 1 ; k <= n ; k++ )
 			for ( int i = 1 ; i <= n ; i++ )
 				for ( int j = 1 ; j <= n ; j++ )
