d10095d1adc17fb476bd774e59fbb664abd1140e
miohitokiri5474 <lltzpp>
2023-11-29 22:42:40 +0800

    solved some problem

---
+++ C++/ZJ/i212.cpp
@@ -0,0 +1,204 @@
+/*
+                                                                                  ```....``                                                                               
+                                                                       `-:/+osyyhhhhhhhhhhhhyso/:.`                                                                       
+                                                                 .:/oyhhhhyyyyyyyyyyyyyyyyyyyyyyyyhyo/:.                                                                  
+                                                            .:+shhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhys/-`                                                             
+                                                        -/oyhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyys/-                                                          
+                                                    ./shhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyo:`                                                      
+                                                 ./shyyyyyyyyyyyyyyyyyyyyyyysso++/::--......-://+osyyyyyyyyyyyyyyyyys:`                                                   
+                                              `:syyyyyyyyyyyyyyyyyyyyyso+/-.```                  ``.-:+ossyyyyyyyyyyyyyo:`                                                
+                                            -os+/yyyyyyyyyyyyyyyss+/-.`                                 ``-:+osyyyyyyys/+so-                                              
+                                         `:so/:-/syyyyyyyyyyso/:.`                                           ``-/+syyyyo::/os/`                                           
+                                       `/so:---:syyyyyyyys+:``                `.-::::::::::::::---.`              `-/osys+:::+so-                                         
+                                     `/so:---:+syyyyyso/.`             `.-:///:-.`````       ````..-:/::-.`          `.:+ssssosyhs-                                       
+                                   `/yy+///+ossyyyyo/.`            .-://-.``      .o+-      `+o/`     ``.-:/:-.          `-+syyyyyhs:                                     
+                                  :yhyyysssyyyyys+-`           `-:/:.`   -/.     :o:-++.   :o/.:o:`   -+o:  `.-/:-.         .:osyyyyhs:                                   
+                                .ohyyyyyyyyyyys/.           .://-.     `++:++:..o/.```-+/-o/.```.:o::o/-.++`    `-:/:-        `.+syyyyhs-                                 
+                               /yyyyyyyyyyyys/.          `://.``++/:` .o/.```-:/-```````-/-````````::.````:o- `-+++``-/:-`       ./syyyyho.                               
+                             .shyyyyyyyyyys+.         `-//-`  .o:.-:/+o-`````.`````````````````````````````.+++:../o.  `-//-`      `:oyyyhy/                              
+                            :yyyyyyyyyyyso-         .:+/+:.  :+.````````````.:.``````````````````````````````.`````-o:  ./:-/:.      `:syyyhs.                            
+                          `+hyyyyyyyyyys:`        -//..o::/++/``````````````-/:```````````````````````````.`````````.+++/-o: .:/-`     `/syyyh:                           
+                         `ohyyyyyyyyys/`       `-+/. :o-````.```````````.```-//-`..```````````````````````+.``````````.````+/` `-//.     .+syyh+`                         
+                        .shyyyyyyyys+.       `-+o:``++``````````````````:.``-///.--```````````````````````:s````````````````/o../:.//.     -oyyhs.                        
+                       .yhyyyyyyyys-        -+:/o:/+:``````````````````/+-``-///:-:```````````````````````.h/````````````````-+/-/o.`//.    `+syhy.                       
+                      .yyyyyyyyys+`       -+/`/o.``````````````````````oo:``-//////-```````````````````````y+-````````.```````````-o/`.+/`    :syhh.                      
+                     .yhyyyyyyys/`      `++``++````````````..``````````ys/-`-//////:```````````````````````h.o`````````-.```````````:o/++o-    .oyhh.                     
+                    `yhyyyyyyys:       :o++/+/```````````./.``````````-oy/:`.///////.``````````````````````h`+-```````/`--.```````````:``.+/`   `oyhy.                    
+                   `shyyyyyyys:      .o+..`..```````````-o-..`````````o.o+/-`///////:````````````-````````.h -o```````o/`./+``````````````.:o`   `oyhy`                   
+                   ohyyyyyyys:      :o:.```````````````:o:-.``./.````:+`:h//.:///////-```````````s````````:y-.y```````/-+`./o```````````````-o.   `oyho                   
+                  /hyyyyyyys:     `+o..```````````````-s/-.``:o+````.s::.oo/:-////////``````````-s:```````o:`-y:.`````-/.o`-+o```````````````-o-   .syd/                  
+                 -dyyyyyyyy/     `o/..```````````````.s/:.`:/:+```.:s:`  `s//:////////:`````````o-+```````y`  +/-::-.`.o -o`-+o```````````````.s.   :syh.                 
+                `yhyyyyyyyo     `s/.`````````````````o+:-:/..o-::::s-     .s///////////-```````-h`o``````//   /:```--:/s.`//`:o+```````````````.s.   oyhs                 
+                +hyyyyyyys.    `s:.`````````````````-y/++:-/o:-.`.+.       ./+++++////++:.----:o/ o--...-+`   //------:+..`s.-/s-```````````````-o`  .syd:                
+               .dyyyyyyys:    `s:.``````````````````os/.`` /////:-`          ``..---::::----..``  `......     `..-.--..`   .::+oy````````````````:+   /yhh/`              
+               sdyyyyyyyo     o/.``````````:````````y`                                                                          +/````````````````+:  `sydhy-             
+              :hyyyyyyys.    /o.```````````+-``````.s                                                                           .y```````:````````.y   /yhhyho.           
+            .ohyyyyyyyy+    .y.````````````:+``````:o                                                                            y.`````-/`````````y-  .sydyyyh/          
+           /hyyyyyyyyyy-    o/.`````````````y.`````:+                                                                            +:`````o.`````````o+   oydyyyyhs.        
+         `shyyyyyyyyyyo    .y.``````````````:s`````:o`ooooooooo+++//::-.`````                              ```.--:::///++++++/-  //````.s``````````/s   /yhhyyyyyh:       
+        -hhyyyyyyyyyyy/    +o-```````````````++````-s-ddddddddddddddddddddhyy/                            /yhdddddddddddhhhhhho  /+````s-``````````-y   .yydyyyyyyh+      
+       :hyyyyyyyyyyyyy-    y//````````````````++```.y ```````....--::/+hdddddo                            /hdddh+-.`````         //```o:```````````.h`  `sydyyyyyyyho`    
+      /dyyyyyyyyyyyyys`   .s:+`````````````````/o.``s`             `/sdddds/`               `      `        -/sdddho:`           o-``o:`````````````h`   oyhhyyyyyyyhs    
+     /dyyyyyyyyyyyyyyo    :+/+``````````````````.o:`//          `/sdddho:`           `    ``      `      `     `-+ydddy/.       `y`-o-`````````-````y.   /yhhyyyyyyyyho   
+    /dyyyyyyyyyyyyyyy+    //+/````````````````````++-o       `:sdddho:`                  ``      `      `          ./sdddy/.    -+/o``````````::````o/   :yhhyyyyyyyyyh+  
+   :dyyyyyyyyyyyyyyyy/    +/s-````````````````````:/`-`   `-ohddds:`               .-:-.`      ``  `-::::.            `:ohddy/` :-`o``````````s`````-y   :yhdyyyyyyyyyyd- 
+  -dyyyyyyyyyyyyyyyyy/    /oy``````````````````````y`   -+hddds/`               `////.-+//+:`    -+s:./:.oo`             `-ohdo   `s`````````/+``````y:  :yydyyyyyyyyyyyy`
+ `yhyyyyyyyyyyyyyyyyy+    -d+``````````````````````+:  -hddy/.`                 o++/://:/+:/o/:/+++::/::/::s`         ```````.`   -o````````-s```````-y` /yhdyyyyyyyyyyyh/
+ +dyyyyyyyyyyyyyyyyyyo    `h.``````````````````````.y`  -/.````````````````    -o::::::::::::::::::::::::::y-   ``````````````````s-```````-s.````````+s oyhhoyhyyyyyyyyh+
+`dyyyyyyyyyyyyyyyyyyys    ++````````````````````````:s` `````````````````````` :+::::::::::::::::::::::::::y.  ``````````````````:o```````/o```````````oossdo `.:+syhyyhy.
+-dyyyyyyyyyyyyyyhyyyyy.  -s``````````````````````````/o``````````````````````` `s::::::::::++/:-:/+/::::::/o   `````````````````-y.`````.o/``./:````````+hyd-      ``..`` 
+ oyhhhhyyyso+/:-hyyyyy/ .y.`````````.-`````--`````````+o`````````````````````   -++/::///+/.`     `://////:`     ``````````````-s-`````/+.`````-//:``````:yh`             
+  ````````      ohyyyys.s-`````````:+```````-/-````````/o.````````````````       ``-:--.``           `````                    -o.```./+-`````````.-s+:-```.+o.            
+                .dyyyyys.````````:+:``````````:+:.``````-o-````````                                                         `//```:+/.`````````````h`.:so/:--//-`         
+                 shyyyo.``````.:s:``````````````-//:.````.+/`                                                             `-/-.-:sh:`.````````````:s  `syd:-://++-        
+                 -dys:`````.://-o.`````````````.-``-/++:-.`-/:`                                                           `.````/h:`.:````````````s/  +yho                
+                 .o/.`.-:+/:.`  -+``````````````:.````+/-:///+o/-`                                                            `oy-``:.```````````.h` -syy`                
+               .//:+sssyyy:      y.`````````````.:````.s`  ```````                                                           -y+.``-:````````````o+ `syh.                 
+                   `yhyyyys-     :o``````````````--````sh/.                                                                .os:```-/.``.````````:y``+yh-                  
+                    `shyyyys-     o:``````````.```:.```-hyy+.                                                            .+s/````-/:``-.```````.y- /yy-                   
+                     `ohyyyys-    `s-`````````--``.:-```+o-oys:.                                                      `-+o:.````-/:``--````````s+`/hs.                    
+                       :yyyyys:    .s-`````````:-``-/-```s: `:oso:`      ``                                  `     `-/s/-``````://-`-:`..`````oo.+y/`                     
+                        .+yyyys/`   .o:`````````::.`-/-``.s:   .:oso:.``:++:`     .:`      ..       ./.    `/+/..-/+:-s``````.://:.:/.`o.````oo:so.                       
+                          ./yyyyo.   `+/````````.::.`-/:.`.s:     `-+ooso-.:+-``-/:-o:   `/+++.   `:o-+/``:o/-+o:-.  /:`.-``.:///-:/-`/y```.ssos-                         
+                            `syyys/`   /+.```-/```:/:.-//-..s:        `-/+ooohss/-```++.:+-  -+/.-o/..-+o/+:-.`     .o`:+/`-////://-`/s+``-yhs:`                          
+                             o/-+yhs:`  .+/```s+.``-:/:://:-.o+`           `.-:/++++++oss/::::/sso/-...``          `o:/-+::///////:.//o.`+hh-                             
+                             o:``.:oys:`  -+-`-o+:``.://////:-/o.                     `````  ```./+.              .so/``y////////:-+://-o//o                              
+                             y-`````.:oo+:.`:/-:+:+/.`-://////:/s/`                      `..`     :y            `:s+.  +o//+o///:+s+ss++.`/o                              
+                            `h``````````-/+o+//+/o--/+:.-:///os+/oo:`                  `+///++    .y            .-`   :s/+os+//oyyoyy/.```/o                              
+                            :o``````````````-/+osyho--:/+/:///+soo+oo:`                `++/++/   -s-                 -s+so+o/+/:..--``````-y                              
+                            s:```````````````....-:/+oooossyyysssso:-:.                  ``.``.:+/.           ```.-:+yssos+::``````````````h.                             
+                           -y```````````````............--:://+++oosyo+///::::::::://++++++++oss+///::::::/+oo++++//:-.......``````````````o+                             
+                           s:```````````````.......................:y```..---------..+:.```   .+` ``````````-s................`````````````-h`                            
+                          /s```````````````........................:hs/.`             :/.````.o`            `y................``````````````o/                            
+                         .y.````..````````...............:........ohyyyyso/-``       `//y++++y:/`      `.:+oyy/....--..........`````````````.h.                           
+                        `s/````:s`````````.............../......-yyyyyyyyyyyyso+:.``  ..-+++o/..  `.:+oyyyyyyyho..../..........``````````-```:y`                          
+                        o+````+oo````````../............+-.....:yyyyyyyyyyyyyyyhooss+::o--s+o`:/:oys+oyyyyyyyyyys-..::..........`````````:+```+o                          
+                       +o```-o-+/```````../o...........:+.....:hyyyyyyyyyyyyyyyy::yyyyyyo::h-:oyyyyy::yyyyyyyyyyyy-..o...........-````````os.``o+                         
+                      +o``.++` y.``````..:h-...........s-....:hyyyyyyyyyyyyyyyh+:+hyyyyyyyyhydhhyyyh+:/hyyyyyyyyyyy-.//..........+-````````yo/``o+                        
+                    `s/`.+o.  :o``````..-ho...........+/....-hyyyyyyyyyyyyyyyyy::yyyyyyhhhhdhhhyyyyyy::syyyyyyyyyyyy..+-..........o-```````-s:o-`++                       
+                   -s--++.    y-`````..-ys-........../o.....yyyyyyyyyyyyyyyyyyo::hyyyyyydhhyyyyyyyyyh/:/hyyyyyyyyyyhs.:/-.........-y:```````/+`/o./s`                     
+                 .oo++/`     :o`````..-y:o..........:+:....+hyyyyyyyyhyyyyyyyh/:+hyyyyyyyyyyyyyyyyyyho::yyyyhyyyyyyyh+.//..........:h/```````+/ `+o/s:                    
+               .oh+/.       `y.```...:y.s-.........-/+....-hyyyyyyyyyhyyyyyyyh::ohyyyyyyyyyyyyyyyyyyyy::oyyyhyyyyyyyyh-.//..........+y+```````o:  `/+so.                  
+             `--`           s:```.../s`//.........-/+.....syyyyyyyyyyhyyyyyyyy::syyyyyyyyyyyyyyyyyyyyh/:/hyyyhyyyyyyyyy.:-/..........+oo-``````o/    `:+/`                
+*/
+
+/* Gura is cute >///< */
+
+/* by. MiohitoKiri5474 */
+
+#include<bits/stdc++.h>
+
+#pragma GCC optimize ( "O3" )
+#pragma loop_opt ( on )
+
+using namespace std;
+
+// define pair
+typedef long long LL;
+typedef pair < LL, LL > pll;
+typedef pair < int, int > pii;
+#define F first
+#define S second
+#define mp make_pair
+
+// general defines
+#define pb push_back
+#define ep emplace_back
+#define endl '\n'
+#define GL ( n ) getline ( cin, n )
+
+// define heap
+template < class T > using MaxHeap = priority_queue < T, vector < T >, less < T > >;
+template < class T > using MinHeap = priority_queue < T, vector < T >, greater < T > >;
+
+// define stringstream
+#define sstr stringstream
+
+// number~ remember change maxN
+#define INF 0x3f3f3f3f
+#define maxN 100005
+
+string add ( string a, string b ) {
+	string res;
+	int carry = 0;
+	for ( int i = 0 ; i < a.size() ; i++ ) {
+		carry += a[i] - '0' + b[i] - '0';
+		res += '0' + ( carry % 10 );
+		carry /= 10;
+	}
+	for ( int i = a.size() ; i < b.size() ; i++ ) {
+		carry += b[i] - '0';
+		res += '0' + ( carry % 10 );
+		carry /= 10;
+	}
+
+	return res;
+}
+
+int main() {
+	ios::sync_with_stdio ( false );
+	cin.tie ( 0 );
+	cout.tie ( 0 );
+
+	string a, b, ans;
+	char c;
+	int carry = 0;
+	cin >> a >> c >> b;
+	reverse ( a.begin(), a.end() );
+	reverse ( b.begin(), b.end() );
+	if ( c == '+' ) {
+		if ( a.size() > b.size() )
+			swap ( a, b );
+		ans = add ( a, b );
+	}
+	else if ( c == '-' ) {
+		for ( int i = 0 ; i < b.size() ; i++ ) {
+			carry += a[i] - b[i];
+			ans += '0' + ( carry + 10 ) % 10;
+			if ( carry < 0 )
+				carry = -1;
+			else
+				carry = 0;
+		}
+		for ( int i = b.size() ; i < a.size() ; i++ ) {
+			carry += a[i] - '0';
+			ans += '0' + ( ( carry + 10 ) % 10 );
+			if ( carry < 0 )
+				carry = -1;
+			else
+				carry = 0;
+		}
+		while ( ans.back() == '0' )
+			ans.pop_back();
+	}
+	else {
+		for ( int i = 0 ; i < a.size() ; i++ ) {
+			string tmp;
+			for ( int j = 0 ; j < i ; j++ )
+				tmp += '0';
+
+			for ( int j = 0 ; j < b.size() ; j++ ) {
+				carry += ( a[i] - '0' ) * ( b[j] - '0' );
+				tmp += '0' + ( carry % 10 );
+				carry /= 10;
+			}
+			ans = add ( ans, tmp );
+		}
+	}
+	if ( ans.size() == 0 )
+		ans = "0";
+	reverse ( ans.begin(), ans.end() );
+	cout << ans << endl;
+}
+
+/*
+321
+654
+	123
+*	456
+-------
+    738
+   6150
+  49200
+--------
+ */
