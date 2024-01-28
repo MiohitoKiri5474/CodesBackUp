ba8472385c58f80c45cc4aeda1bef2f1e34583eb
miohitokiri5474 <lltzpp>
2024-01-28 18:36:29 +0800

    Solved some problem (CF, TOJ)

--- C++/TOJ/047.cpp
+++ C++/TOJ/047.cpp
@@ -12,10 +12,12 @@ int main(){
 	vector < int > data;
 	int n, t;
 	cin >> n;
+
+	int arr[105];
 	while ( n-- ){
 		cin >> t;
 		if ( check.find ( t ) == check.end() ){
-			check.insert ( t );
+			// check.insert ( t );
 			data.push_back ( t );
 		}
 	}
@@ -26,19 +28,24 @@ int main(){
 	while ( t-- ){
 		cin >> n;
 		auto i = lower_bound ( data.begin(), data.end(), n );
+		int *j = lower_bound ( arr + 3, arr + 15, t );
 
 		if ( *i == n )
 			cout << n;
-		else if ( n < *data.begin() )
-			cout << "no " << *i;
-		else if ( *data.rbegin() < n )
-			cout << *data.rbegin() << " no";
+		else if ( n < data[0] )
+			cout << "no " << data[0];
+		else if ( data.back() < n )
+			cout << data.back() << " no";
 		else{
-			i--;
-			cout << *i << ' ';
-			i++;
-			cout << *i;
+			cout << *( i - 1 ) << ' ' << *i;
 		}
 		cout << '\n';
 	}
 }
+
+// |_____|_____|
+//      mid
+// target > mid
+//
+//
+//
