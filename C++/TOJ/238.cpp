7e5a0d0cfbfd49d1e6e156149938b190d2aa5b40
MiohitoKiri5474 <lltzpp@gmail.com>
2025-03-29 11:52:37 +0800

    Rewrite: TOJ 238

--- C++/TOJ/238.cpp
+++ C++/TOJ/238.cpp
@@ -1,101 +1,66 @@
 // by. MiohitoKiri5474
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 #pragma GCC optimize ( "O3" )
 #pragma loop_opt ( on )
 
 using namespace std;
 
-typedef long long LL;
-
-// define tools
-#define REPP(i,f,s) for ( int i = f ; i < s ; i++ )
-#define REPM(i,f,s) for ( int i = f ; i >= s ; i-- )
-#define REPALL(i,n) for ( auto &i: n )
-#define debuger cout << "111\n"
-#define SP(a,b) swap ( a, b )
-#define MEM(n,i) memset ( n, i, sizeof n )
-
-// define pair
-typedef pair < LL, LL > pll;
-typedef pair < int, int > pii;
+// type and pair definition
+using LL = long long;
+using pii = pair < LL, LL >;
+using sstr = stringstream;
 #define F first
 #define S second
 #define mp make_pair
 
-// define vector && some stl's api
-template < class T > using vec = vector < T >;
-typedef vec < int > vi;
-typedef vec < LL > vl;
+// general definition
 #define pb push_back
 #define ep emplace_back
-#define REV reverse
-#define SZ(n) ( int ) n.size()
-#define CLR(n) n.clear()
-#define BEG(n) n.begin()
-#define END(n) n.end()
-#define EMP(n) n.empty()
-#define RSZ(n,s) n.resize ( s )
-#define ALL(n) BEG ( n ), END ( n )
-#define PIO(n) REPALL ( i, n ) cout << i << ' '; cout << '\n'
-
-// define set
-typedef set < int > si;
-typedef set < LL > sl;
-#define FID(n,Index) n.find ( Index ) != n.end()
-
-// graph
-#define GRE(T,edge) vec < T > edge[maxN]
-#define UNI(u,v,edge) edge[u].pb ( v ), edge[v].pb ( u )
-#define UNIw(u,v,w,edge) edge[u].pb ( mp ( v, w ) ), edge[v].pb ( mp ( u, w ) )
-
-// IO
 #define GL(n) getline ( cin, n )
+#define gura ios::sync_with_stdio(false);cin.tie(0)
 
-// define stack, queue, pri-queue
-template < class T > using stack = stack < T, vec < T > >;
-template < class T > using MaxHeap = priority_queue < T, vec < T >, less < T > >;
-template < class T > using MinHeap = priority_queue < T, vec < T >, greater < T > >;
+// define heap
+template < class T > using MaxHeap = priority_queue < T, vector < T >, less < T > >;
+template < class T > using MinHeap = priority_queue < T, vector < T >, greater < T > >;
 
-// define stringstream
-#define sstr stringstream
-
-// number~ remember change maxN
+// number definition
 #define INF 0x3f3f3f3f
-#define maxN 10005
 
-// ready~ go!
-// let's coding and have fun!
-// I can solve this problem!
+// some defiine for programing contest
+#define int LL
+inline void print_ans ( bool flag ) {
+    cout << ( flag ? "Yes" : "No" ) << endl;
+}
+const int maxN = 100005;
 
-typedef pair < short, short > pss;
-
-GRE ( pss, edge );
 int mi, ma;
+vector < pii > edges[maxN];
 
-inline bool dfs ( short n, short p, int sum ){
-	if ( mi <= sum && sum <= ma )
-		return true;
-	REPALL ( i, edge[n] ){
-		if ( i.F == p )
-			continue;
-		if ( dfs ( i.F, n, sum + i.S ) )
-			return true;
-	}
+bool dfs ( int n, int p, int sum ) {
+    if ( mi <= sum && sum <= ma )
+        return true;
 
-	return false;
+    for ( auto [to, w]: edges[n] ) {
+        if ( to == p )
+            continue;
+        if ( dfs ( to, n, sum + w ) )
+            return true;
+    }
+
+    return false;
 }
 
-int main(){
-	ios::sync_with_stdio ( false );
-	cin.tie ( 0 );
-	cout.tie ( 0 );
+signed main() {
+    gura;
 
-	short n, u, v, w;
-	cin >> n >> mi >> ma;
-	REPP ( i, 1, n ){
-		cin >> u >> v >> w;
-		UNIw ( u, v, w, edge );
-	}
-	cout << ( dfs ( 0, -1, 0 ) ? "Eat Eat Eat" : "Go find other chickens" ) << '\n';
+    int n, u, v, w;
+    cin >> n >> mi >> ma;
+    for ( int i = 1 ; i < n ; i++ ) {
+        cin >> u >> v >> w;
+        edges[u].pb ( pii ( v, w ) );
+        edges[v].pb ( pii ( u, w ) );
+    }
+
+    cout << ( dfs ( 0, -1, 0 ) ? "Eat Eat Eat\n" : "Go find other chickens\n" );
 }
