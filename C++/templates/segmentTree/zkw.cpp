14fe4a0d036762818f5b984d5fee693af81145ea
MiohitoKiri5474 <lltzpp@gmail.com>
2025-06-03 17:51:30 +0800

    Update ZKW with RSQ

--- C++/templates/segmentTree/zkw.cpp
+++ C++/templates/segmentTree/zkw.cpp
@@ -15,21 +15,20 @@ struct ZKW {
         seg.resize(base << 1, 0);
 
         memcpy (&seg[base--], v.data(), n * sizeof(int));
-        for (int i = base; i > 0; i--) {
-            seg[i] = max (seg[i << 1], seg[i << 1 | 1]);
-        }
+        for (int i = base; i > 0; i--)
+            seg[i] = seg[i << 1] + seg[i << 1 | 1];
     }
 
     inline void update ( int pos, int val) {
         for (seg[pos += base] = val, pos >>= 1; pos > 1; pos >>= 1)
-            seg[pos] = max (seg[pos << 1], seg[pos << 1 | 1]);
+            seg[pos] = seg[pos << 1] + seg[pos << 1 | 1];
     }
 
     inline int query (int l, int r) {
-        int res = -2147483648;
+        int res = 0;
         for (l += base, r += base; l < r; l >>= 1, r >>= 1) {
-            if (l & 1) res = max (res, seg[l++]);
-            if (r & 1) res = max (res, seg[--r]);
+            if (l & 1) res += seg[l++];
+            if (r & 1) res += seg[--r];
         }
         return res;
     }
