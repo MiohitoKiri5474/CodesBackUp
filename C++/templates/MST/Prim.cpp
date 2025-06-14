0a2eca19510b50f076d482ee394e7d0b0babb671
MiohitoKiri5474 <lltzpp@gmail.com>
2025-06-14 10:27:32 +0800

    styling

--- C++/templates/MST/Prim.cpp
+++ C++/templates/MST/Prim.cpp
@@ -1,6 +1,6 @@
 // Prim
 // by. MiohitoKiri5474
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #include <queue>
 
 using namespace std;
@@ -8,67 +8,61 @@ using namespace std;
 #define pb push_back
 #define F first
 #define S second
-typedef pair < int, int > pii;
-typedef pair < int, pii > pipii;
+typedef pair<int, int> pii;
+typedef pair<int, pii> pipii;
 const int maxN = 100005;
 
 int dis[maxN], sz[maxN];
 
-inline void init ( void ){
-	for ( int i = 0 ; i < maxN ; i++ )
+inline void init(void) {
+	for (int i = 0; i < maxN; i++)
 		dis[i] = i, sz[i] = 1;
 }
 
-int find ( int n ){
-	return dis[n] == n ? n : dis[n] = find ( dis[n] );
-}
+int find(int n) { return dis[n] == n ? n : dis[n] = find(dis[n]); }
 
-inline void Union ( int a, int b ){
-	a = find ( a ), b = find ( b );
+inline void Union(int a, int b) {
+	a = find(a), b = find(b);
 	dis[a] = b;
 	sz[b] += sz[a];
 }
 
-inline bool same ( int a, int b ){
-	return find ( a ) == find ( b );
-}
+inline bool same(int a, int b) { return find(a) == find(b); }
 
-vector < pii > edges[maxN], mst[maxN];
+vector<pii> edges[maxN], mst[maxN];
 bool pushed[maxN];
 
 struct node {
-    int u, v, w;
-    inline bool operator < ( const node &b ) const {
-        return w < b.w;
-    }
+	int u, v, w;
+	inline bool operator<(const node &b) const { return w < b.w; }
 };
 
-inline void Pirm ( int n ){
-    priority_queue < node, vector < node >, greater < node > > pq;
-	for ( auto [i, dist]: edges[0] )
-		pq.push ( node { 0, i, dist } );
+inline void Pirm(int n) {
+	priority_queue<node, vector<node>, greater<node>> pq;
+	for (auto [i, dist] : edges[0])
+		pq.push(node{0, i, dist});
 	pushed[0] = true;
 	init();
-	while ( sz[find ( 0 )] != n ){
-        auto [u, v, w] = pq.top();
+	while (sz[find(0)] != n) {
+		auto [u, v, w] = pq.top();
 		pq.pop();
-		while ( !pq.empty() && same ( u, v ) ){
-            u = pq.top().u, v = pq.top().v, w = pq.top().w;
+		while (!pq.empty() && same(u, v)) {
+			u = pq.top().u, v = pq.top().v, w = pq.top().w;
 			pq.pop();
 		}
 
-		mst[u].pb ( pii ( v, w ) );
-		mst[v].pb ( pii ( u, w ) );
-		Union ( u, v );
+		mst[u].pb(pii(v, w));
+		mst[v].pb(pii(u, w));
+		Union(u, v);
 
-		if ( !pushed[u] ){
-			for ( auto [i, dist]: edges[u] )
-                pq.push ( node { u, i, dist } );
+		if (!pushed[u]) {
+			for (auto [i, dist] : edges[u])
+				pq.push(node{u, i, dist});
 			pushed[u] = true;
 		}
-		if ( !pushed[v] ){
-			for ( auto [i, dist]: edges[v] )
-                pq.push ( node { v, i, dist } );
+		if (!pushed[v]) {
+			for (auto [i, dist] : edges[v])
+				pq.push(node{v, i, dist});
 			pushed[v] = true;
 		}
 	}
