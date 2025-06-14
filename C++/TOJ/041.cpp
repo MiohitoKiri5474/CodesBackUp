40038a40027e18218108f16126b6e8e0432bd4de
MiohitoKiri5474 <lltzpp@gmail.com>
2025-06-14 11:11:29 +0800

    Update TOJ/041 with Prim

--- C++/TOJ/041.cpp
+++ C++/TOJ/041.cpp
@@ -1,135 +1,102 @@
 // by. MiohitoKiri5474
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 #pragma GCC optimize ( "O3" )
 #pragma loop_opt ( on )
 
 using namespace std;
 
-typedef long long LL;
-
-// define tools
-#define REPP(i,f,s) for ( int i = f ; i < s ; i++ )
-#define REPM(i,f,s) for ( int i = f ; i >= s ; i-- )
-#define REPALL(i,n) for ( auto &i: n )
-#define debuger cout << "111\n"
-#define MEM(n,i) memset ( n, i, sizeof n )
-
-// define pair
-typedef pair < LL, LL > pll;
-typedef pair < int, int > pii;
+// type and pair definition
+using LL = long long;
+using pii = pair < LL, LL >;
+using sstr = stringstream;
 #define F first
 #define S second
 #define mp make_pair
 
-// define vector && some stl's api
-template < class T > using vec = vector < T >;
-typedef vec < int > vi;
-typedef vec < LL > vl;
+// general definition
 #define pb push_back
 #define ep emplace_back
-#define REV reverse
-#define SZ(n) ( int ) n.size()
-#define CLR(n) n.clear()
-#define BEG(n) n.begin()
-#define END(n) n.end()
-#define EMP(n) n.empty()
-#define RSZ(n,s) n.resize ( s )
-#define ALL(n) BEG ( n ), END ( n )
-#define PIO(n) REPALL ( i, n ) cout << i << ' '; cout << '\n'
-#define GETDATA(data,n) RSZ ( data, n ); REPALL ( i, data ) cin >> i
-
-// define set
-typedef set < int > si;
-typedef set < LL > sl;
-#define FID(n,Index) ( n.find ( Index ) != n.end() )
-
-// graph
-#define GRE(T,edge) vec < T > edge[maxN]
-#define UNI(u,v,edge) edge[u].pb ( v ), edge[v].pb ( u )
-#define UNIw(u,v,w,edge) edge[u].pb ( mp ( v, w ) ), edge[v].pb ( mp ( u, w ) )
-
-// IO
 #define GL(n) getline ( cin, n )
+#define gura ios::sync_with_stdio(false);cin.tie(0)
 
-// define stack, queue, pri-queue
-template < class T > using stack = stack < T, vec < T > >;
-template < class T > using MaxHeap = priority_queue < T, vec < T >, less < T > >;
-template < class T > using MinHeap = priority_queue < T, vec < T >, greater < T > >;
+// define heap
+template < class T > using MaxHeap = priority_queue < T, vector < T >, less < T > >;
+template < class T > using MinHeap = priority_queue < T, vector < T >, greater < T > >;
 
-// define stringstream
-#define sstr stringstream
-
-// number~ remember change maxN
+// number definition
 #define INF 0x3f3f3f3f
-#define NEG_INF 0x8f8f8f8f
-#define maxN 1005
 
-// あの日見渡した渚を　今も思い出すんだ
-// 砂の上に刻んだ言葉　君の後ろ姿
-// 寄り返す波が　足元をよぎり何かを攫う
-// 夕凪の中　日暮れだけが通り過ぎて行く
+// some defiine for programing contest
+// #define int LL
+inline void print_ans ( bool flag ) {
+    cout << ( flag ? "Yes" : "No" ) << endl;
+}
+const int maxN = 100005;
 
-// ready~ go!
-// let's go coding and have fun!
-// I can solve this problem!
+struct node {
+    int u, v, w;
 
-struct node{
-	int u, v, w;
+    inline bool operator < ( const node &a ) const {
+        return w < a.w;
+    }
+    inline bool operator > ( const node &a ) const {
+        return w > a.w;
+    }
+
+    node (int _u = 0, int _v = 0, int _w = 0) : u(_u), v(_v), w(_w) {}
 };
 
-inline bool cmp ( node a, node b ){
-	return a.w < b.w;
+int dis[maxN];
+vector<pii> edges[maxN];
+bitset<maxN> pushed;
+
+int find (int x) {
+    return dis[x] == x ? x : dis[x] = find(dis[x]);
 }
 
-vec < node > edges;
-int dis[maxN], sz[maxN], ans;
+signed main() {
+    gura;
 
-inline void init ( int k ){
-	REPP ( i, 0, k ){
-		dis[i] = i;
-		sz[i] = 1;
-	}
-}
+    int n, m, ans = 0, cnt = 0;
+    MinHeap<node> pq;
+    cin >> n >> m;
+    for (int i = 0, u, v, w; i < m; i++) {
+        cin >> u >> v >> w;
+        edges[u].pb(mp(v, w));
+        edges[v].pb(mp(u, w));
+    }
 
-int find ( int a ){
-	return dis[a] == a ? a : dis[a] = find ( dis[a] );
-}
+    for (int i = 0; i < n; i++)
+        dis[i] = i;
+    pushed[0] = true;
+    for (auto [i, dist] : edges[0])
+        pq.push(node(0, i, dist));
 
-inline void Union ( int a, int b ){
-	a = find ( a ), b = find ( b );
-	dis[a] = b;
-	sz[b] += sz[a];
-}
+    while (!pq.empty() && cnt < n - 1) {
+        auto [u, v, w] = pq.top();
+        pq.pop();
+        while (!pq.empty() && find(u) == find(v)) {
+            u = pq.top().u;
+            v = pq.top().v;
+            w = pq.top().w;
+            pq.pop();
+        }
 
-inline bool same ( int a, int b ){
-	return find ( a ) == find ( b );
-}
+        cnt++;
+        dis[find(u)] = find(v);
+        ans += w;
+        if (!pushed[u]) {
+            pushed[u] = true;
+            for (auto [i, dist] : edges[u])
+                pq.push(node(u, i, dist));
+        }
+        if (!pushed[v]) {
+            pushed[v] = true;
+            for (auto [i, dist] : edges[v])
+                pq.push(node(v, i, dist));
+        }
+    }
 
-inline void Kruskal ( int k ){
-	init ( k );
-	sort ( ALL ( edges ), cmp );
-	REPALL ( i, edges ){
-		if ( same ( i.u, i.v ) )
-			continue;
-		Union ( i.u, i.v );
-		ans += i.w;
-	}
-}
-
-int main(){
-	ios::sync_with_stdio ( false );
-	cin.tie ( 0 );
-	cout.tie ( 0 );
-
-	int n, m, u, v, w;
-	cin >> n >> m;
-	while ( m-- ){
-		cin >> u >> v >> w;
-		edges.pb ( node { u, v, w } );
-	}
-
-	Kruskal ( n );
-
-	cout << ( sz[find ( 1 )] == n ? ans : -1 ) << '\n';
+    cout << ( cnt == n - 1 ? ans : -1 ) << endl;
 }
