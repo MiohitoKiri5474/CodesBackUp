1e843e93be2d923e439ec907e489ce4b8f367b20
MiohitoKiri5474 <lltzpp@gmail.com>
2025-07-05 10:51:54 +0800

    Rewrite ZJ d767

--- C++/ZJ/d767.cpp
+++ C++/ZJ/d767.cpp
@@ -1,152 +1,98 @@
-/************************************/
-/* Date		: 2019-10-28 19:29:04	*/
-/* Author	: MiohitoKiri5474		*/
-/* Email	: lltzpp@gmail.com		*/
-/************************************/
-
 // by. MiohitoKiri5474
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 #pragma GCC optimize ( "O3" )
 #pragma loop_opt ( on )
 
 using namespace std;
 
-typedef long long LL;
-
-// define tools
-#define REPP(i,f,s) for ( int i = f ; i < s ; ++i )
-#define REPM(i,f,s) for ( int i = f ; i >= s ; --i )
-#define REPALL(i,n) for ( auto &i: n )
-#define debuger cout << "111\n"
-#define MEM(n,i) memset ( n, i, sizeof n )
-
-// define pair
-typedef pair < LL, LL > pll;
-typedef pair < int, int > pii;
+// type and pair definition
+using LL = long long;
+using pii = pair < LL, LL >;
+using sstr = stringstream;
 #define F first
 #define S second
 #define mp make_pair
 
-// define vector && some stl's api
-template < class T > using vec = vector < T >;
-typedef vec < int > vi;
-typedef vec < LL > vl;
+// general definition
 #define pb push_back
-#define eb emplace_back
-#define REV reverse
-#define SZ(n) ( int ) n.size()
-#define CLR(n) n.clear()
-#define BEG(n) n.begin()
-#define END(n) n.end()
-#define EMP(n) n.empty()
-#define RSZ(n,s) n.resize ( s )
-#define ALL(n) BEG ( n ), END ( n )
-#define PIO(n) REPALL ( i, n ) cout << i << ' '; cout << '\n'
-#define GETDATA(data,n) RSZ ( data, n ); REPALL ( i, data ) cin >> i
-
-// define set
-typedef set < int > si;
-typedef set < LL > sl;
-#define FID(n,Index) ( n.find ( Index ) != n.end() )
-
-// graph
-#define GRE(T,edge) vec < T > edge[maxN]
-#define UNI(u,v,edge) edge[u].pb ( v ), edge[v].pb ( u )
-#define UNIw(u,v,w,edge) edge[u].pb ( mp ( v, w ) ), edge[v].pb ( mp ( u, w ) )
-
-// IO
+#define ep emplace_back
 #define GL(n) getline ( cin, n )
+#define gura ios::sync_with_stdio(false);cin.tie(0)
 
-// define stack, queue, pri-queue
-template < class T > using stack = stack < T, vec < T > >;
-template < class T > using MaxHeap = priority_queue < T, vec < T >, less < T > >;
-template < class T > using MinHeap = priority_queue < T, vec < T >, greater < T > >;
+// define heap
+template < class T > using MaxHeap = priority_queue < T, vector < T >, less < T > >;
+template < class T > using MinHeap = priority_queue < T, vector < T >, greater < T > >;
 
-// define stringstream
-#define sstr stringstream
-
-// number~ remember change maxN
+// number definition
 #define INF 0x3f3f3f3f
-#define NEG_INF 0x8f8f8f8f
-#define maxN 30005
-#define maxLog 15
 
-// あの日見渡した渚を　今も思い出すんだ
-// 砂の上に刻んだ言葉　君の後ろ姿
-// 寄り返す波が　足元をよぎり何かを攫う
-// 夕凪の中　日暮れだけが通り過ぎて行く
+// some defiine for programing contest
+#define int LL
+inline void print_ans ( bool flag ) {
+    cout << ( flag ? "Yes" : "No" ) << endl;
+}
+const int maxN = 30005;
+const int maxLog = 19;
 
-// ready~ go!
-// let's go coding and have fun!
-// I can solve this problem!
+vector<int> edges[maxN];
+int dep[maxN], dp[maxN][maxLog];
 
-int dp[maxN][maxLog], D[maxN];
-bool inDeg[maxN];
-GRE ( int, edges );
-
-void dfs ( int n, int dep ){
-	D[n] = dep++;
-	for ( auto i: edges[n] ){
-		dp[i][0] = n;
-		dfs ( i, dep );
-	}
+void dfs (int n, int p, int d) {
+    dep[n] = ++d;
+    dp[n][0] = p;
+    for (auto i: edges[n]) {
+        if (i == p)
+            continue;
+        dfs(i, n, d);
+    }
 }
 
-inline void build ( int n, int s ){
-	memset ( dp, -1, sizeof dp );
-	dfs ( s, 0 );
-	for ( int k = 1 ; k < maxLog ; k++ )
-		for ( int i = 1 ; i <= n ; i++ )
-			if ( dp[i][k - 1] != -1 )
-				dp[i][k] = dp[dp[i][k - 1]][k - 1];
+inline void buildLCA (int n) {
+    memset (dp, -1, sizeof dp);
+    dfs (1, -1, 0);
+    for (int k = 1; k < maxLog; k++)
+        for (int i = 1; i <= n; i++)
+            if (dp[i][k - 1] != -1)
+                dp[i][k] = dp[dp[i][k - 1]][k - 1];
 }
 
-inline int LCA ( int x, int y ){
-	if ( D[x] < D[y] )
-		swap ( x, y );
+signed main() {
+    gura;
 
-	REPM ( i, maxLog - 1, 0 )
-		if ( dp[x][i] != -1 && D[dp[x][i]] >= D[y] )
-			x = dp[x][i];
+    int n, q, u, v, du, dv;
+    cin >> n >> q;
+    for (int i = 1, in; i <= n; i++) {
+        while (cin >> in) {
+            if (!in)
+                break;
+            edges[i].pb(in);
+        }
+    }
 
-	if ( x == y )
-		return x;
+    buildLCA(n);
 
-	REPM ( i, maxLog - 1, 0 )
-		if ( dp[x][i] != dp[y][i] )
-			x = dp[x][i], y = dp[y][i];
+    while (q--) {
+        cin >> u >> v;
 
-	return dp[x][0];
-}
+        if (dep[u] > dep[v])
+            swap(u, v);
 
-int main(){
-	ios::sync_with_stdio ( false );
-	cin.tie ( 0 );
-	cout.tie ( 0 );
+        du = dep[u], dv = dep[v];
 
-	int n, q, u, v, root;
-	cin >> n >> q;
-	for ( int i = 1 ; i <= n ; ++i ){
-		while ( cin >> v ){
-			if ( v == 0 )
-				break;
-			edges[i].pb ( v );
-			inDeg[v] = true;
-		}
-	}
+        for (int k = maxLog - 1; k >= 0; k--)
+            if (dp[v][k] != -1 && dep[dp[v][k]] >= dep[u])
+                v = dp[v][k];
 
-	for ( int i = 1 ; i <= n ; ++i )
-		if ( !inDeg[i] ){
-			root = i;
-			break;
-		}
+        if (u == v) {
+            cout << u << ' ' << dv - du << '\n';
+            continue;
+        }
 
-	build ( n, root );
+        for (int k = maxLog - 1; k >= 0; k--)
+            if (dp[u][k] != dp[v][k])
+                u = dp[u][k], v = dp[v][k];
 
-	while ( q-- ){
-		cin >> u >> v;
-		root = LCA ( u, v );
-		cout << root << ' ' << D[u] + D[v] - 2 * D[root] << '\n';
-	}
+        cout << dp[u][0] << ' ' << du + dv - (dep[dp[u][0]] << 1) << '\n';
+    }
 }
